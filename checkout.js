b9bcede453ba57900c760ee08006e7ac87712d8e
Max Georgopoulos <maxgeorgopoulos@Maxs-MacBook-Air.local>
2022-02-04 19:55:39 -0500

    Added toggle questions for beginners on edit page

--- checkout.js
+++ checkout.js
@@ -1,11 +1,19 @@
-document.querySelector('#showing-preview-checkbox').addEventListener('click', function() {
-  if (document.querySelector('#showing-preview-checkbox').checked === true) {
-      document.querySelector('#manifesto-preview-mobile').style.display = 'block' 
-      document.querySelector('.showing-preview-toggle-nob').style.transform = 'rotate(90deg)' 
-      document.querySelector('.showing-preview-toggle-text').innerHTML = 'Hide Manifesto preview' 
-  } else {
-      document.querySelector('#manifesto-preview-mobile').style.display = 'none';
-      document.querySelector('.showing-preview-toggle-nob').style.transform = 'rotate(0deg)' 
-      document.querySelector('.showing-preview-toggle-text').innerHTML = 'Show Manifesto preview' 
-  }
+const toggleWrappers = document.querySelectorAll('.toggle-wrapper');
+const toggles = Array.from(toggleWrappers).map(el => el.querySelector('input[type="checkbox'));
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('change', () => {
+      const nob = toggle.closest('.toggle-wrapper').querySelector('.toggle-nob');
+      const title = toggle.closest('.toggle-wrapper').querySelector('.toggle-title');
+      const content = toggle.closest('.toggle-wrapper').querySelector('.toggle-content');
+      if (toggle.checked === true) {
+        nob.style.transform = 'rotate(90deg)' 
+        title.innerHTML = 'Hide Manifesto preview' 
+        content.style.display = 'block' 
+      } else {
+        nob.style.transform = 'rotate(0deg)' 
+        title.innerHTML = 'Show Manifesto preview' 
+        content.style.display = 'none' 
+      }  
+    });
 });
